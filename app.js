a3695ccd5cfff083e3f67b0d1b5b7c066dc76e48
Abdul Moiz <abdulmoiz15000@gmail.com>
2022-07-04 20:47:01 +0500

    Maths Method

---
+++ app.js
@@ -0,0 +1,68 @@
+//----------------------------------- Q#01 ----------------------------------------------
+// var userNumber=prompt("Enter any positive decimal number");
+// console.log(userNumber);
+// console.log(Math.round(userNumber));
+// console.log(Math.floor(userNumber));
+// console.log(Math.ceil(userNumber));
+
+//----------------------------------- Q#2 ----------------------------------------------
+
+// var userNumber=prompt("Enter any negative decimal number");
+// console.log(userNumber);
+// console.log(Math.round(userNumber));
+// console.log(Math.floor(userNumber));
+// console.log(Math.ceil(userNumber));
+
+//----------------------------------- Q#3 ----------------------------------------------
+
+//  var userNumber=prompt("Enter any value to get its absolute value");
+//  console.log("Absolute value of "+userNumber +" is "+Math.abs(userNumber));
+
+// if(userNumber<0){   
+//    userNumber=(-(userNumber))
+// }
+// else{   
+//     userNumber
+// }
+// console.log(userNumber);
+
+//----------------------------------- Q#4 ----------------------------------------------
+
+// var Dice=Math.random()*5;
+// var M=Math.round(Dice)
+// var d=[1,2,3,4,5,6]
+// console.log("Random dice number is: "+ d[M]);
+
+//----------------------------------- Q#5 ----------------------------------------------
+
+// var Dice=Math.random();
+// var M=Math.round(Dice)
+// console.log(M);
+// var d=["Head","Tail"]
+// console.log("Coin faces: "+d[M]);
+
+//----------------------------------- Q#6 ----------------------------------------------
+
+// var num=Math.random()*100;
+// var N=Math.round(num)
+// console.log("Random number between 1 to 100 is: "+N);
+
+//----------------------------------- Q#7 ----------------------------------------------
+
+//  var userWeight=prompt("Enter your weight");
+// //  console.log(parseFloat(userWeight));
+//  console.log(Number(userWeight));
+//  console.log(typeof userWeight);
+
+//----------------------------------- Q#8 ----------------------------------------------
+
+// var ranNum=Math.random()*10;
+// var R=Math.round(ranNum)
+//  var userNum=+prompt("Enter any num between 1 to 10");
+//  console.log(R);
+// if(userNum===R){
+//     console.log("Congrats buddy..");
+// }
+// else{
+//     console.log("Try Again!");
+// }
